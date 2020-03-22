b710aeda493463ef191e9af9963beda8dbd9af45
Isuru <pathumkodippili@gmail.com>
2020-03-22 12:53:28 +0530

    2020-03-22-01

--- app/src/main/java/com/example/fitnes_app/MainActivity.java
+++ app/src/main/java/com/example/fitnes_app/MainActivity.java
@@ -10,4 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void Loading(String loading) {
+        System.out.println("loading.....");
+    }
 }
